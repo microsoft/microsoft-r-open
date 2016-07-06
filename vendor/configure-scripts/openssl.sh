eaf007ab440b47863540162cfcdfdd5af905378a
Nathan Sosnovske <nsosnov@microsoft.com>
2016-07-06 14:49:30 -0700

    Update openssl flags

--- vendor/configure-scripts/openssl.sh
+++ vendor/configure-scripts/openssl.sh
@@ -2,4 +2,4 @@ export LDFLAGS=-L${1}/build/lib
 export CFLAGS="-I${1}/build/include -fPIC"
 export CPPFLAGS="-I${1}/build/include -fPIC"
 
-${1}/${2}/Configure no-shared --prefix=${1}/build linux-x86_64 
+${1}/${2}/Configure no-shared --prefix=${1}/build -fPIC linux-x86_64 
