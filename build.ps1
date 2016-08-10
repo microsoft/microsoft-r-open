9c2c0107fc0b4af7a315cc007d19d4e952f770ca
Nathan Sosnovske <nsosnov@microsoft.com>
2016-08-10 00:52:08 +0000

    Updated build.ps1

--- build.ps1
+++ build.ps1
@@ -29,7 +29,7 @@ if($WhatIf.IsPresent) {
 
 # Try download NuGet.exe if do not exist.
 if (!(Test-Path $NUGET_EXE)) {
-    Invoke-WebRequest -Uri http://nuget.org/nuget.exe -OutFile $NUGET_EXE
+    Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/v3.5.0-beta2/NuGet.exe -OutFile $NUGET_EXE
 }
 
 # Make sure NuGet exists where we expect it.
