29ed3737477df3123d939f65fb2a145ac555339d
Aaron Grider <aagrider@microsoft.com>
2016-08-30 18:20:47 +0000

    PR 5949: Fix fontconfig confdir configuration path
     - Fix fontconfig confdir configuration path

--- vendor/configure-scripts/fontconfig.sh
+++ vendor/configure-scripts/fontconfig.sh
@@ -3,4 +3,10 @@ export CFLAGS="-I${1}/build/include -fPIC"
 export CPPFLAGS="-I${1}/build/include -fPIC"
 export PKG_CONFIG_PATH=${1}/build/lib/pkgconfig
 
-${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared
+# If we are building for Mac we need to override the conf dir
+if [ "${OSTYPE//[0-9.]/}" == "darwin" ]
+then
+    ${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared --sysconfdir=/opt/X11/lib/X11/
+else
+    ${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared
+fi
