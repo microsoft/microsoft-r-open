83f0a380fc87f5974a7df61701e6ba8fdcb0dd53
Nathan Sosnovske <nsosnov@microsoft.com>
2016-10-26 18:13:05 +0000

    PR 9938: Add support for unicode properties in pcre
     - Add support for unicode properties in pcre

--- vendor/configure-scripts/pcre.sh
+++ vendor/configure-scripts/pcre.sh
@@ -2,4 +2,4 @@ export LDFLAGS=-L${1}/build/lib
 export CFLAGS="-I${1}/build/include -fPIC"
 export CPPFLAGS="-I${1}/build/include -fPIC"
 
-${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared --enable-utf
+${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared --enable-utf --enable-unicode-properties
