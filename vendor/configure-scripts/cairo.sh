5b5de5ea007a5e7d9e668962b828d4db13299098
Nathan Sosnovske <nsosnov@microsoft.com>
2016-11-28 15:02:47 -0800

    PR 8527: Merge aagrider/linkerFixes to internal
     - Remove unneeded Core Framework flags
     - Fix make dependencies
     - Add cairo param
     - Only use CoreFrameworks for C compilation
     - Readd make
     - Remove CoreFoundation links

--- vendor/configure-scripts/cairo.sh
+++ vendor/configure-scripts/cairo.sh
@@ -1,7 +1,7 @@
 export LDFLAGS=-L${1}/build/lib
-export CFLAGS="-I${1}/build/include -fPIC -framework CoreFoundation -framework CoreGraphics -framework CoreText"
-export CPPFLAGS="-I${1}/build/include -fPIC -framework CoreFoundation -framework CoreGraphics -framework CoreText"
+export CFLAGS="-I${1}/build/include -fPIC"
+export CPPFLAGS="-I${1}/build/include -fPIC"
 export PKG_CONFIG_PATH=${1}/build/lib/pkgconfig
 
 
-${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared
+${1}/${2}/configure --prefix=${1}/build --enable-static --disable-shared --disable-quartz
--- vendor/configure-scripts/png.sh
+++ vendor/configure-scripts/png.sh
@@ -3,8 +3,8 @@ export LDFLAGS=-L${1}/build/lib
 # If we are building for Mac we need to set Core Framworks
 if [ "${OSTYPE//[0-9.]/}" == "darwin" ]
 then
-    export CFLAGS="-I${1}/build/include -fPIC -framework CoreFoundation -framework CoreGraphics -framework CoreText"
-    export CPPFLAGS="-I${1}/build/include -fPIC -framework CoreFoundation -framework CoreGraphics -framework CoreText"
+    export CFLAGS="-I${1}/build/include -fPIC"
+    export CPPFLAGS="-I${1}/build/include -fPIC"
 else
     export CFLAGS="-I${1}/build/include -fPIC"
     export CPPFLAGS="-I${1}/build/include -fPIC"
