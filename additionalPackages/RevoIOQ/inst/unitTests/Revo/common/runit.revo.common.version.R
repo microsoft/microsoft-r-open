f833d012cb657b764d86b16376d41de522ab5db1
Rich Calaway <richcala@microsoft.com>
2019-05-15 16:46:56 -0700

    Changes for MRO-3.5.3

--- additionalPackages/RevoIOQ/inst/unitTests/Revo/common/runit.revo.common.version.R
+++ additionalPackages/RevoIOQ/inst/unitTests/Revo/common/runit.revo.common.version.R
@@ -26,7 +26,7 @@ if (!identical(system.file(package="RevoMods"), ""))
     "test.revor.RevoIOQ.version" <- function() checkTrue("checkPackageVersion"(pkg = "RevoIOQ", ver = revover))
     "test.revor.RUnit.version" <- function() checkTrue("checkPackageVersion"(pkg = "RUnit", ver = "0.4.26"))
     "test.revoparallelr.doParallel.version" <- function() checkTrue("checkPackageVersion"(pkg = "doParallel", ver = "1.0.14"))
-    "test.revor.RevoUtils.version" <- function() checkTrue("checkPackageVersion"(pkg = "RevoUtils", ver = "11.0.2"))
+    "test.revor.RevoUtils.version" <- function() checkTrue("checkPackageVersion"(pkg = "RevoUtils", ver = "11.0.3"))
     "test.revor.RevoMods.version" <- function() checkTrue("checkPackageVersion"(pkg = "RevoMods", ver = "11.0.1"))
 } else
 {
